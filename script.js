a7ae2b76dc73c92d488a65bcaeec943a8558f492
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-01 12:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function stringChop(str, size) {
   // your code here
-}
+	let iterations = Math.floor(str.length / size);
+	let index = 0;
+	let res = [];
+	for (let i = 0; i < iterations; i++) {
+		res.push(str.slice(index,index+size));
+		index += size;
+	}
 
+	if (str.length - index > 0) {
+		res.push(str.slice(index,str.length));
+	}
+
+	
+	return res;
+}
+// stringChop("12345",2);
+// stringChop("abc",5);
 // Do not change the code below
+
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
